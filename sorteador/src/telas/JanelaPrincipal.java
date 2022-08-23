22f28b44d66a93f59dcb558cce6c9fa999a1205d
charl <charl@Charles>
2022-08-22 23:06:17 -0300

    Mais um

--- sorteador/src/telas/JanelaPrincipal.java
+++ sorteador/src/telas/JanelaPrincipal.java
@@ -21,16 +21,19 @@ import org.jsoup.select.Elements;
 
 import aposta.Jogo;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 
 public class JanelaPrincipal extends JFrame {
 
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 1L;
-
+	private QuadroNumeros panelQuadroNumeros;
 	private JPanel contentPane;
 	private LinkedList<Jogo> todosJogo;
 
@@ -56,32 +59,30 @@ public class JanelaPrincipal extends JFrame {
 	 */
 	public JanelaPrincipal() {
 		iniciaComponentes();
-		
+
 		this.todosJogo = new LinkedList<Jogo>();
 
 		Document doc;
-		
+
 		try {
 			File input = new File("caixa.html");
-			
-			if(!input.exists()) {
+
+			if (!input.exists()) {
 				System.out.println("ALllll");
 			}
-			
+
 			doc = Jsoup.parse(input, "UTF-8", "");
-			
-			
-			
+
 			Element content = doc.getElementsByClass("tabela-resultado lotofacil").first();
-			//Elements links = content.getElementsByTag("a");
+			// Elements links = content.getElementsByTag("a");
 			// Elements content2 = content.getElementsByClass("tabela-resultado lotofacil");
-			
+
 			for (int i = 1; i < content.childrenSize(); i++) {
-				
+
 				Elements elements = content.child(i).children().first().children();
-	
+
 				Jogo jogo = new Jogo(elements.get(0).html(), elements.get(1).html());
-				
+
 				jogo.adicionaNumero(elements.get(2).html());
 				jogo.adicionaNumero(elements.get(3).html());
 				jogo.adicionaNumero(elements.get(4).html());
@@ -97,7 +98,7 @@ public class JanelaPrincipal extends JFrame {
 				jogo.adicionaNumero(elements.get(14).html());
 				jogo.adicionaNumero(elements.get(15).html());
 				jogo.adicionaNumero(elements.get(16).html());
-				
+
 				System.out.println(elements.get(0).html());
 				System.out.println(elements.get(1).html());
 				System.out.println(elements.get(2).html());
@@ -114,44 +115,53 @@ public class JanelaPrincipal extends JFrame {
 				System.out.println(elements.get(13).html());
 				System.out.println(elements.get(14).html());
 				System.out.println(elements.get(15).html());
-				System.out.println(elements.get(16).html());	
-				
+				System.out.println(elements.get(16).html());
+
 				todosJogo.add(jogo);
 			}
-			
-		/*	for (Element element : content.children()) {
-				content.c
-				for (Element element2 : element.children().first().children()) {
-					System.out.println(element2.html());
-				}
-			}
-			*/
-			
+
+			/*
+			 * for (Element element : content.children()) { content.c for (Element element2
+			 * : element.children().first().children()) {
+			 * System.out.println(element2.html()); } }
+			 */
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void iniciaComponentes() {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 617, 373);
-		
+
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-
-		QuadroNumeros panel = new QuadroNumeros();
-		contentPane.add(panel);
 		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		panelQuadroNumeros = new QuadroNumeros();
+		tabbedPane.addTab("Números", null, panelQuadroNumeros, null);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Mais Jogados", null, panel, null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setColumns(80);
+		textArea.setRows(20);
+		panel.add(textArea);
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 
 		JMenu menuArquivo = new JMenu("Arquivo");
 		menuBar.add(menuArquivo);
-		
+
 		JMenuItem menuItem01 = new JMenuItem("Resultado Sorteio");
 		menuItem01.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -159,14 +169,144 @@ public class JanelaPrincipal extends JFrame {
 			}
 		});
 		menuArquivo.add(menuItem01);
+		
+		JMenuItem menuItem02 = new JMenuItem("Mais Jogados");
+		menuItem02.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menuItem02Action(e);
+			}
+		});
+		menuArquivo.add(menuItem02);
+	}
+
+	public void menuItem01Action(ActionEvent e) {
+		int sorteio = Integer.valueOf(JOptionPane.showInputDialog("Número do concurso")) - 1;
+	
+		System.out.println(todosJogo.size());
+		
+		if (sorteio > 0 && sorteio < todosJogo.size()) {
+			System.out.println(todosJogo.get(sorteio).getApostaNumeros());
+			
+			for (Integer numero : todosJogo.get(sorteio).getApostaNumeros()) {
+				colorirLabels(numero);
+			}
+		}else {
+			System.out.println("NÃO EXISTE!");
+		}
+	}
+
+	public void menuItem02Action(ActionEvent e) {
+		System.out.println("SUsusus");
 	}
 	
-	public void menuItem01Action(ActionEvent e) {
-		int sorteio = 2586;
-		
-		System.out.println(todosJogo.get(sorteio).getApostaNumeros());
-		
-		System.out.println("Entrou!!");
+	public void descolorirLabels() {
+		panelQuadroNumeros.getLabelNumero01().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero02().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero03().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero04().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero05().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero06().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero07().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero08().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero09().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero10().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero11().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero12().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero13().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero14().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero15().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero16().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero17().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero18().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero19().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero20().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero21().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero22().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero23().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero24().setBackground(Color.WHITE);
+		panelQuadroNumeros.getLabelNumero25().setBackground(Color.WHITE);
+	}
+
+	public void colorirLabels(int n) {
+		switch (n) {
+		case 1:
+			panelQuadroNumeros.getLabelNumero01().setBackground(new Color(255, 200, 0));
+			break;
+		case 2:
+			panelQuadroNumeros.getLabelNumero02().setBackground(new Color(255, 200, 0));
+			break;
+		case 3:
+			panelQuadroNumeros.getLabelNumero03().setBackground(new Color(255, 200, 0));
+			break;
+		case 4:
+			panelQuadroNumeros.getLabelNumero04().setBackground(new Color(255, 200, 0));
+			break;
+		case 5:
+			panelQuadroNumeros.getLabelNumero05().setBackground(new Color(255, 200, 0));
+			break;
+		case 6:
+			panelQuadroNumeros.getLabelNumero06().setBackground(new Color(255, 200, 0));
+			break;
+		case 7:
+			panelQuadroNumeros.getLabelNumero07().setBackground(new Color(255, 200, 0));
+			break;
+		case 8:
+			panelQuadroNumeros.getLabelNumero08().setBackground(new Color(255, 200, 0));
+			break;
+		case 9:
+			panelQuadroNumeros.getLabelNumero09().setBackground(new Color(255, 200, 0));
+			break;
+		case 10:
+			panelQuadroNumeros.getLabelNumero10().setBackground(new Color(255, 200, 0));
+			break;
+		case 11:
+			panelQuadroNumeros.getLabelNumero11().setBackground(new Color(255, 200, 0));
+			break;
+		case 12:
+			panelQuadroNumeros.getLabelNumero12().setBackground(new Color(255, 200, 0));
+			break;
+		case 13:
+			panelQuadroNumeros.getLabelNumero13().setBackground(new Color(255, 200, 0));
+			break;
+		case 14:
+			panelQuadroNumeros.getLabelNumero14().setBackground(new Color(255, 200, 0));
+			break;
+		case 15:
+			panelQuadroNumeros.getLabelNumero15().setBackground(new Color(255, 200, 0));
+			break;
+		case 16:
+			panelQuadroNumeros.getLabelNumero16().setBackground(new Color(255, 200, 0));
+			break;
+		case 17:
+			panelQuadroNumeros.getLabelNumero17().setBackground(new Color(255, 200, 0));
+			break;
+		case 18:
+			panelQuadroNumeros.getLabelNumero18().setBackground(new Color(255, 200, 0));
+			break;
+		case 19:
+			panelQuadroNumeros.getLabelNumero19().setBackground(new Color(255, 200, 0));
+			break;
+		case 20:
+			panelQuadroNumeros.getLabelNumero20().setBackground(new Color(255, 200, 0));
+			break;
+		case 21:
+			panelQuadroNumeros.getLabelNumero21().setBackground(new Color(255, 200, 0));
+			break;
+		case 22:
+			panelQuadroNumeros.getLabelNumero22().setBackground(new Color(255, 200, 0));
+			break;
+		case 23:
+			panelQuadroNumeros.getLabelNumero23().setBackground(new Color(255, 200, 0));
+			break;
+		case 24:
+			panelQuadroNumeros.getLabelNumero24().setBackground(new Color(255, 200, 0));
+			break;
+		case 25:
+			panelQuadroNumeros.getLabelNumero25().setBackground(new Color(255, 200, 0));
+			break;
+		default:
+			break;
+		}
 	}
 
 }
--- sorteador/src/telas/QuadroNumeros.java
+++ sorteador/src/telas/QuadroNumeros.java
@@ -10,6 +10,32 @@ import javax.swing.SwingConstants;
 
 public class QuadroNumeros extends JPanel {
 
+	private JLabel labelNumero01;
+	private JLabel labelNumero02;
+	private JLabel labelNumero03;
+	private JLabel labelNumero04;
+	private JLabel labelNumero05;
+	private JLabel labelNumero06;
+	private JLabel labelNumero07;
+	private JLabel labelNumero08;
+	private JLabel labelNumero09;
+	private JLabel labelNumero10;
+	private JLabel labelNumero11;
+	private JLabel labelNumero12;
+	private JLabel labelNumero13;
+	private JLabel labelNumero14;
+	private JLabel labelNumero15;
+	private JLabel labelNumero16;
+	private JLabel labelNumero17;
+	private JLabel labelNumero18;
+	private JLabel labelNumero19;
+	private JLabel labelNumero20;
+	private JLabel labelNumero21;
+	private JLabel labelNumero22;
+	private JLabel labelNumero23;
+	private JLabel labelNumero24;
+	private JLabel labelNumero25;
+	
 	/**
 	 * Create the panel.
 	 */
@@ -17,175 +43,175 @@ public class QuadroNumeros extends JPanel {
 		setBackground(Color.GRAY);
 		setLayout(new GridLayout(5, 5, 5, 5));
 
-		JLabel labelNumero01 = new JLabel("01");
+		labelNumero01 = new JLabel("01");
 		labelNumero01.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero01.setBackground(Color.WHITE);
 		labelNumero01.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero01.setOpaque(true);
 		add(labelNumero01);
 
-		JLabel labelNumero02 = new JLabel("02");
+		labelNumero02 = new JLabel("02");
 		labelNumero02.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero02.setBackground(Color.WHITE);
 		labelNumero02.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero02.setOpaque(true);
 		add(labelNumero02);
 
-		JLabel labelNumero03 = new JLabel("03");
+		labelNumero03 = new JLabel("03");
 		labelNumero03.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero03.setBackground(Color.WHITE);
 		labelNumero03.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero03.setOpaque(true);
 		add(labelNumero03);
 
-		JLabel labelNumero04 = new JLabel("04");
+		labelNumero04 = new JLabel("04");
 		labelNumero04.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero04.setBackground(Color.WHITE);
 		labelNumero04.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero04.setOpaque(true);
 		add(labelNumero04);
 
-		JLabel labelNumero05 = new JLabel("05");
+		labelNumero05 = new JLabel("05");
 		labelNumero05.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero05.setBackground(Color.WHITE);
 		labelNumero05.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero05.setOpaque(true);
 		add(labelNumero05);
 
-		JLabel labelNumero06 = new JLabel("06");
+		labelNumero06 = new JLabel("06");
 		labelNumero06.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero06.setBackground(Color.WHITE);
 		labelNumero06.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero06.setOpaque(true);
 		add(labelNumero06);
 
-		JLabel labelNumero07 = new JLabel("07");
+		labelNumero07 = new JLabel("07");
 		labelNumero07.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero07.setBackground(Color.WHITE);
 		labelNumero07.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero07.setOpaque(true);
 		add(labelNumero07);
 
-		JLabel labelNumero08 = new JLabel("08");
+		labelNumero08 = new JLabel("08");
 		labelNumero08.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero08.setBackground(Color.WHITE);
 		labelNumero08.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero08.setOpaque(true);
 		add(labelNumero08);
 
-		JLabel labelNumero09 = new JLabel("09");
+		labelNumero09 = new JLabel("09");
 		labelNumero09.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero09.setBackground(Color.WHITE);
 		labelNumero09.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero09.setOpaque(true);
 		add(labelNumero09);
 
-		JLabel labelNumero10 = new JLabel("10");
+		labelNumero10 = new JLabel("10");
 		labelNumero10.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero10.setBackground(Color.WHITE);
 		labelNumero10.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero10.setOpaque(true);
 		add(labelNumero10);
 
-		JLabel labelNumero11 = new JLabel("11");
+		labelNumero11 = new JLabel("11");
 		labelNumero11.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero11.setBackground(Color.WHITE);
 		labelNumero11.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero11.setOpaque(true);
 		add(labelNumero11);
 
-		JLabel labelNumero12 = new JLabel("12");
+		labelNumero12 = new JLabel("12");
 		labelNumero12.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero12.setBackground(Color.WHITE);
 		labelNumero12.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero12.setOpaque(true);
 		add(labelNumero12);
 
-		JLabel labelNumero13 = new JLabel("13");
+		labelNumero13 = new JLabel("13");
 		labelNumero13.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero13.setBackground(Color.WHITE);
 		labelNumero13.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero13.setOpaque(true);
 		add(labelNumero13);
 
-		JLabel labelNumero14 = new JLabel("14");
+		labelNumero14 = new JLabel("14");
 		labelNumero14.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero14.setBackground(Color.WHITE);
 		labelNumero14.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero14.setOpaque(true);
 		add(labelNumero14);
 
-		JLabel labelNumero15 = new JLabel("15");
+		labelNumero15 = new JLabel("15");
 		labelNumero15.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero15.setBackground(Color.WHITE);
 		labelNumero15.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero15.setOpaque(true);
 		add(labelNumero15);
 
-		JLabel labelNumero16 = new JLabel("16");
+		labelNumero16 = new JLabel("16");
 		labelNumero16.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero16.setBackground(Color.WHITE);
 		labelNumero16.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero16.setOpaque(true);
 		add(labelNumero16);
 
-		JLabel labelNumero17 = new JLabel("17");
+		labelNumero17 = new JLabel("17");
 		labelNumero17.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero17.setBackground(Color.WHITE);
 		labelNumero17.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero17.setOpaque(true);
 		add(labelNumero17);
 
-		JLabel labelNumero18 = new JLabel("18");
+		labelNumero18 = new JLabel("18");
 		labelNumero18.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero18.setBackground(Color.WHITE);
 		labelNumero18.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero18.setOpaque(true);
 		add(labelNumero18);
 
-		JLabel labelNumero19 = new JLabel("19");
+		labelNumero19 = new JLabel("19");
 		labelNumero19.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero19.setBackground(Color.WHITE);
 		labelNumero19.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero19.setOpaque(true);
 		add(labelNumero19);
 
-		JLabel labelNumero20 = new JLabel("20");
+		labelNumero20 = new JLabel("20");
 		labelNumero20.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero20.setBackground(Color.WHITE);
 		labelNumero20.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero20.setOpaque(true);
 		add(labelNumero20);
 
-		JLabel labelNumero21 = new JLabel("21");
+		labelNumero21 = new JLabel("21");
 		labelNumero21.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero21.setBackground(Color.WHITE);
 		labelNumero21.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero21.setOpaque(true);
 		add(labelNumero21);
 
-		JLabel labelNumero22 = new JLabel("22");
+		labelNumero22 = new JLabel("22");
 		labelNumero22.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero22.setBackground(Color.WHITE);
 		labelNumero22.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero22.setOpaque(true);
 		add(labelNumero22);
 
-		JLabel labelNumero23 = new JLabel("23");
+		labelNumero23 = new JLabel("23");
 		labelNumero23.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero23.setBackground(Color.WHITE);
 		labelNumero23.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero23.setOpaque(true);
 		add(labelNumero23);
 
-		JLabel labelNumero24 = new JLabel("24");
+		labelNumero24 = new JLabel("24");
 		labelNumero24.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero24.setBackground(Color.WHITE);
 		labelNumero24.setFont(new Font("Arial", Font.PLAIN, 30));
 		labelNumero24.setOpaque(true);
 		add(labelNumero24);
 
-		JLabel labelNumero25 = new JLabel("25");
+		labelNumero25 = new JLabel("25");
 		labelNumero25.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNumero25.setBackground(Color.WHITE);
 		labelNumero25.setFont(new Font("Arial", Font.PLAIN, 30));
@@ -193,4 +219,203 @@ public class QuadroNumeros extends JPanel {
 		add(labelNumero25);
 	}
 
+	public JLabel getLabelNumero01() {
+		return labelNumero01;
+	}
+
+	public void setLabelNumero01(JLabel labelNumero01) {
+		this.labelNumero01 = labelNumero01;
+	}
+
+	public JLabel getLabelNumero02() {
+		return labelNumero02;
+	}
+
+	public void setLabelNumero02(JLabel labelNumero02) {
+		this.labelNumero02 = labelNumero02;
+	}
+
+	public JLabel getLabelNumero03() {
+		return labelNumero03;
+	}
+
+	public void setLabelNumero03(JLabel labelNumero03) {
+		this.labelNumero03 = labelNumero03;
+	}
+
+	public JLabel getLabelNumero04() {
+		return labelNumero04;
+	}
+
+	public void setLabelNumero04(JLabel labelNumero04) {
+		this.labelNumero04 = labelNumero04;
+	}
+
+	public JLabel getLabelNumero05() {
+		return labelNumero05;
+	}
+
+	public void setLabelNumero05(JLabel labelNumero05) {
+		this.labelNumero05 = labelNumero05;
+	}
+
+	public JLabel getLabelNumero06() {
+		return labelNumero06;
+	}
+
+	public void setLabelNumero06(JLabel labelNumero06) {
+		this.labelNumero06 = labelNumero06;
+	}
+
+	public JLabel getLabelNumero07() {
+		return labelNumero07;
+	}
+
+	public void setLabelNumero07(JLabel labelNumero07) {
+		this.labelNumero07 = labelNumero07;
+	}
+
+	public JLabel getLabelNumero08() {
+		return labelNumero08;
+	}
+
+	public void setLabelNumero08(JLabel labelNumero08) {
+		this.labelNumero08 = labelNumero08;
+	}
+
+	public JLabel getLabelNumero09() {
+		return labelNumero09;
+	}
+
+	public void setLabelNumero09(JLabel labelNumero09) {
+		this.labelNumero09 = labelNumero09;
+	}
+
+	public JLabel getLabelNumero10() {
+		return labelNumero10;
+	}
+
+	public void setLabelNumero10(JLabel labelNumero10) {
+		this.labelNumero10 = labelNumero10;
+	}
+
+	public JLabel getLabelNumero11() {
+		return labelNumero11;
+	}
+
+	public void setLabelNumero11(JLabel labelNumero11) {
+		this.labelNumero11 = labelNumero11;
+	}
+
+	public JLabel getLabelNumero12() {
+		return labelNumero12;
+	}
+
+	public void setLabelNumero12(JLabel labelNumero12) {
+		this.labelNumero12 = labelNumero12;
+	}
+
+	public JLabel getLabelNumero13() {
+		return labelNumero13;
+	}
+
+	public void setLabelNumero13(JLabel labelNumero13) {
+		this.labelNumero13 = labelNumero13;
+	}
+
+	public JLabel getLabelNumero14() {
+		return labelNumero14;
+	}
+
+	public void setLabelNumero14(JLabel labelNumero14) {
+		this.labelNumero14 = labelNumero14;
+	}
+
+	public JLabel getLabelNumero15() {
+		return labelNumero15;
+	}
+
+	public void setLabelNumero15(JLabel labelNumero15) {
+		this.labelNumero15 = labelNumero15;
+	}
+
+	public JLabel getLabelNumero16() {
+		return labelNumero16;
+	}
+
+	public void setLabelNumero16(JLabel labelNumero16) {
+		this.labelNumero16 = labelNumero16;
+	}
+
+	public JLabel getLabelNumero17() {
+		return labelNumero17;
+	}
+
+	public void setLabelNumero17(JLabel labelNumero17) {
+		this.labelNumero17 = labelNumero17;
+	}
+
+	public JLabel getLabelNumero18() {
+		return labelNumero18;
+	}
+
+	public void setLabelNumero18(JLabel labelNumero18) {
+		this.labelNumero18 = labelNumero18;
+	}
+
+	public JLabel getLabelNumero19() {
+		return labelNumero19;
+	}
+
+	public void setLabelNumero19(JLabel labelNumero19) {
+		this.labelNumero19 = labelNumero19;
+	}
+
+	public JLabel getLabelNumero20() {
+		return labelNumero20;
+	}
+
+	public void setLabelNumero20(JLabel labelNumero20) {
+		this.labelNumero20 = labelNumero20;
+	}
+
+	public JLabel getLabelNumero21() {
+		return labelNumero21;
+	}
+
+	public void setLabelNumero21(JLabel labelNumero21) {
+		this.labelNumero21 = labelNumero21;
+	}
+
+	public JLabel getLabelNumero22() {
+		return labelNumero22;
+	}
+
+	public void setLabelNumero22(JLabel labelNumero22) {
+		this.labelNumero22 = labelNumero22;
+	}
+
+	public JLabel getLabelNumero23() {
+		return labelNumero23;
+	}
+
+	public void setLabelNumero23(JLabel labelNumero23) {
+		this.labelNumero23 = labelNumero23;
+	}
+
+	public JLabel getLabelNumero24() {
+		return labelNumero24;
+	}
+
+	public void setLabelNumero24(JLabel labelNumero24) {
+		this.labelNumero24 = labelNumero24;
+	}
+
+	public JLabel getLabelNumero25() {
+		return labelNumero25;
+	}
+
+	public void setLabelNumero25(JLabel labelNumero25) {
+		this.labelNumero25 = labelNumero25;
+	}
 }
